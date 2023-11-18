c472264a6597d317adc5f1269a1791288924899f
Mahdi <khoshdellmahdi@gmail.com>
2023-11-18 08:58:29 +0330

    added news-app models

--- johar/settings/base.py
+++ johar/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^52)m%csca16l(doj^=+mx@p0)*(m0%)$*g2r@uqbj29a@!3ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- news/models.py
+++ news/models.py
@@ -7,3 +7,71 @@ class Subscriber(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Agency(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    # todo add other fields
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=250)
+    icon = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class News(models.Model):
+    title = models.CharField(max_length=50)
+    agency = models.ForeignKey('Agency', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='news')
+    description = models.CharField(max_length=250)
+    icon = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.agency.user.username} + {self.title}'
+
+
+class Like(models.Model):
+    news = models.ForeignKey('News', on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.news
+
+
+class DisLike(models.Model):
+    news = models.ForeignKey('News', on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.news
+
+
+class Comment(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    news = models.ForeignKey('News', on_delete=models.CASCADE)
+    user = models.ForeignKey('Subscriber', on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    # todo add update?
+
+    def __str__(self):
+        return self.news
