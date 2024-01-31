eccdd27f22ceebb4270608bea66396760f93d3be
kasra <kasra.hajian@divar.ir>
2024-01-31 04:50:20 +0330

    Read ALLOWED_HOSTS from environment variables

--- johar/settings/production.py
+++ johar/settings/production.py
@@ -5,9 +5,14 @@ import dj_database_url
 from johar.settings.base import *  # NOQA
 
 DEBUG = False
-ALLOWED_HOSTS = [
-    'backend',
-]
+
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
